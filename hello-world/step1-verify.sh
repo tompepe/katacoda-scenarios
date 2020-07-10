a03a4adbfbd41603c04ab2567be84bb5416be19f
dev1 <dev1@spr-ml1.attlocal.net>
2020-07-09 22:20:23 -0500

    apparently verify must run on one line

--- hello-world/step1-verify.sh
+++ hello-world/step1-verify.sh
@@ -1,5 +1,5 @@
-echo "import React from 'react'; function AppFake(){return(<a>I am learning react</a>)} export default AppFake;" > /root/my-app/src/AppFake.js
-cat /root/my-app/src/App.test.js | sed 's/App/AppFake/g' > /root/my-app/src/AppFake.test.js
-CI=true npm test /root/my-app/src/AppFake.test.js -- --json --outputFile=fake-test-run.json
-[[ "$(cat fake-test-run.json | jq '.success' | grep -o -i -c '^true$')" = "1" ]] || (rm -f /root/my-app/src/AppFake*; rm -f /root/my-app/fake-test-run.json; false)
+echo "import React from 'react'; function AppFake(){return(<a>I am learning react</a>)} export default AppFake;" > /root/my-app/src/AppFake.js && \
+cat /root/my-app/src/App.test.js | sed 's/App/AppFake/g' > /root/my-app/src/AppFake.test.js && \
+CI=true npm test /root/my-app/src/AppFake.test.js -- --json --outputFile=fake-test-run.json && \
+[[ "$(cat fake-test-run.json | jq '.success' | grep -o -i -c '^true$')" = "1" ]] || (rm -f /root/my-app/src/AppFake*; rm -f /root/my-app/fake-test-run.json; false) && \
 [[ "$(cat fake-test-run.json | jq '.success' | grep -o -i -c '^true$')" = "1" ]] && (rm -f /root/my-app/src/AppFake*; rm -f /root/my-app/fake-test-run.json; echo "done")
--- hello-world/step1.md
+++ hello-world/step1.md
@@ -6,7 +6,6 @@ This is an _example_ of a simple react app
 
 `cd my-app'`{{execute}}
 
-
 Make the tests fail 
 1. open my-app/src/App.test.js 
 2. change the 'renders learn react link' test to verify that the page says 'I can learn react'
